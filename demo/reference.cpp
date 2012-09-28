8f47c91f21f4ae2007b8974a7abaf3ded91da901
ipa-tys <tys@ipa.fhg.de>
2012-09-28 21:05:19 +0200

    ticket 400 completed

--- demo/reference.cpp
+++ demo/reference.cpp
@@ -15,35 +15,17 @@ int main(int argc, char *argv[]) {
     std::cout << "Cannot open CAN device; aborting." << std::endl;
     return -1;
   } 
- 
-  // initialize listener thread:
+
   canopen::initListenerThread();    
-
-  // put NMT and 402 state machines for device  to operational:
-  // canopen::faultReset(deviceID);
-
-  canopen::sendSDO(deviceID, "controlword", "disable_voltage");
   std::this_thread::sleep_for(std::chrono::milliseconds(50)); 
-
-  std::cout << "hi1" << std::endl;
   canopen::initNMT();
-  std::cout << "hi2" << std::endl;
   if (!canopen::initDevice(deviceID, std::chrono::milliseconds(10))) { 
     std::cout << "Device could not be initialized; aborting." << std::endl;
     return -1;
   } 
 
-  auto ans = canopen::sendSDO(deviceID, "statusword");
-  std::cout << "Status switched_on? " << ans->checkForConstant("switched_on") << std::endl;
-
-
   canopen::moveUntilUserInterrupt(deviceID, direction);
-  // canopen::homing(deviceID);
-  canopen::shutdownDevice(deviceID);
-
-  // canopen::sendSDO(deviceID, "controlword", "disable_voltage");
-  std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
-
   canopen::homing(deviceID);
-  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+  return 0;
 }
