4d7edc5982caf2eadcf8a9439837cb6b367bf221
ipa-tys <tys@ipa.fhg.de>
2012-10-10 19:23:54 +0200

    adapted homing example

--- examples/homing.cpp
+++ examples/homing.cpp
@@ -1,24 +1,31 @@
+// Copyright (c) 2012 Fraunhofer Institute
+// for Manufacturing Engineering and Automation (IPA)
+// See the file license.txt for copying permission.
+
 #include <canopen_highlevel.h>
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    std::cout << "Please call the program with the CAN deviceID"
-	      << "e.g. './single_device 12'" << std::endl;
+  if (argc != 3) {
+    std::cout << "Arguments:" << std::endl
+      	      << "(1) device file" << std::endl
+	      << "(2) CAN deviceID" << std::endl
+	      << "e.g. './homing /dev/pcan32 12'" << std::endl;
     return -1;
   }
-  uint16_t deviceID = std::stoi(std::string(argv[1]));
+  std::string deviceFile = std::string(argv[1]);
+  uint16_t deviceID = std::stoi(std::string(argv[2]));
 
   // initialize CAN device driver:
-  if (!canopen::openConnection("/dev/pcan32")) {
+  if (!canopen::openConnection(deviceFile)) {
     std::cout << "Cannot open CAN device; aborting." << std::endl;
     return -1;
   } 
- 
+    
   // initialize listener thread:
   canopen::initListenerThread();    
   canopen::initNMT();
-  canopen::setSyncInterval(deviceID, std::chrono::milliseconds(10));
   canopen::setMotorState(deviceID, "operation_enable");
   canopen::homing(deviceID);
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  canopen::closeConnection();
 }
